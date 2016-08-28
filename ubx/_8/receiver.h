2f2f77becdb9e9957cf52731a14e86bbab4a2506
Thomas Voß <thomas.voss.bochum@gmail.com>
2016-08-28 22:52:52 +0200

    Apply licensing notice.

--- ubx/_8/receiver.h
+++ ubx/_8/receiver.h
@@ -1,3 +1,17 @@
+// Copyright (C) 2016 Thomas Voss <thomas.voss.bochum@gmail.com>
+// 
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef UBX_8_RECEIVER_H_
 #define UBX_8_RECEIVER_H_
 
@@ -15,7 +29,7 @@ namespace _8
 /// @brief Receiver connects to a ublox 8 GNSS receiver.
 class Receiver : public std::enable_shared_from_this<Receiver>
 {
-  public:       
+  public:
     /// @brief create returns a new Receiver instance connected to the
     /// serial port reachable under dev.
     static std::shared_ptr<Receiver> create(const boost::filesystem::path& dev);
